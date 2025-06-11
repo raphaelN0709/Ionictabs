49d1b80639e37b4703fd592830f9b9614e3c69bd
Raphael <raphael.gabriel@etec.sp.gov.br>
2025-06-11 12:46:19 -0300

    criação Ifilme.ts mudança html e TS tab1

--- tabs/filme/src/app/tab1/tab1.page.ts
+++ tabs/filme/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFilme } from '../model/IFilme';
 
 @Component({
   selector: 'app-tab1',
@@ -9,5 +10,18 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  listafilmes: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h50m',
+      classificacao: 9,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRzR4fti2tdQF0.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat',
+      favorito: false
+    }
+  ];
 
 }
